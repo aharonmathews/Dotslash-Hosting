74f37417698d43f6ef18262444f3dfba92da00e4
Ivin Mathew Kurian <127372364+Ivin-Mathew@users.noreply.github.com>
2025-02-20 05:45:42 +0000

    Set-up base NextJs project

---
+++ dotslash-frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      You can start working here
+    </div>
+    </>
+  );
+}
