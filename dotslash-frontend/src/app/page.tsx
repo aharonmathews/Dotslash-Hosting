947a10ae7b63fa120fd5e5cfdeecda0522eae2eb
adithyanrlal <151168365+adithyanrlal@users.noreply.github.com>
2025-03-06 18:33:47 +0530

    section created

--- dotslash-frontend/src/app/page.tsx
+++ dotslash-frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
+import Collaborate from "./components/Collaborate.jsx"
+
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center h-screen w-full">
-      You can start working here
-    </div>
+      <div className="flex flex-col items-center justify-center h-screen w-full">
+        You can start working here
+      </div>
+      <Collaborate />
     </>
   );
 }
