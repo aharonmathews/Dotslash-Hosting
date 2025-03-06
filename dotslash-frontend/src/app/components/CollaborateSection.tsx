4de02ddc45df06b2c255d7d57b7f98fae2ffee2b
adithyanrlal <151168365+adithyanrlal@users.noreply.github.com>
2025-03-06 23:54:46 +0530

    Made Collaborate responsive

--- dotslash-frontend/src/app/components/CollaborateSection.tsx
+++ dotslash-frontend/src/app/components/CollaborateSection.tsx
@@ -2,22 +2,22 @@ import { Unbounded, Noto_Sans } from 'next/font/google';
 
 const unbounded = Unbounded({ weight: '400', style: 'normal' });
 const notoSans = Noto_Sans({ weight: '400', style: 'normal' });
-const unboundedBold = Unbounded({ weight: '700', style: 'normal' });
 const CollaborateSection = () => {
     return (
-        <section className="bg-black  text-white py-16 px-8 h-screen w-full">
+        <section className="bg-black  text-white py-12 md:py-16 px-8 min-h-screen w-full flex flex-col justify-center">
             {/* Headline */}
             <div className="mb-16">
-                <h2 className={`text-8xl ${unbounded.className} uppercase  mb-24`}>COLLABORATE<br />WITH US</h2>
+                <h2 className={`text-5xl sm:text-6xl md:text-7xl lg:text-8xl  ${unbounded.className} uppercase mb-8 lg:mb-12 2xl:pl-16`}>COLLABORATE<br />WITH US</h2>
             </div>
 
             {/* Two-column cards */}
-            <div className="grid  grid-cols-2 gap-6 justify-evenly max-w-4xl mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2  gap-8 justify-evenly max-w-7xl mx-auto ">
                 {/* Sponsors Card */}
-                <div className="bg-[#D1A83A] text-black p-8  rounded-lg relative group cursor-pointer ">
-                    <h3 className={`text-4xl ${unbounded.className} mb-4`}>For Sponsors</h3>
-                    <p className={`text-base ${notoSans.className} mb-8 `}>Lorem ipsum dolor sit amet consectetur. Ut nec gravida nulla purus. Dui lectus venenatis ut erat. Amet faucibus tincidunt ornare volutpat.</p>
-                    <br />
+                <div className="bg-[#D1A83A] text-black p-8  rounded-lg relative group cursor-pointer h-auto md:h-96 ">
+                    <h3 className={`text-2xl md:text-3xl lg:text-4xl ${unbounded.className} mb-4`}>For Sponsors</h3>
+                    <p className={`text-sm md:text-base ${notoSans.className} mb-8 `}>Lorem ipsum dolor sit amet consectetur. Ut nec gravida nulla purus. Dui lectus venenatis ut erat. Amet faucibus tincidunt ornare volutpat.
+
+                    </p>
                     <br />
                     <br />
                     <br />
@@ -42,9 +42,14 @@ const CollaborateSection = () => {
                 </div>
 
                 {/* Clubs Card */}
-                <div className="bg-[#D1A83A] text-black p-8  rounded-lg relative group cursor-pointer">
-                    <h3 className={`text-4xl ${unbounded.className} mb-4`}>For Clubs</h3>
-                    <p className={`text-base ${notoSans.className} mb-8 `}>Lorem ipsum dolor sit amet consectetur. Ut nec gravida nulla purus. Dui lectus venenatis ut erat. Amet faucibus tincidunt ornare volutpat.</p>
+                <div className="bg-[#D1A83A] text-black p-8  rounded-lg relative group cursor-pointer h-auto md:h-96">
+                    <h3 className={`text-2xl md:text-3xl lg:text-4x ${unbounded.className} mb-4`}>For Clubs</h3>
+                    <p className={`text-sm md:text-base ${notoSans.className} mb-8 `}>Lorem ipsum dolor sit amet consectetur. Ut nec gravida nulla purus. Dui lectus venenatis ut erat. Amet faucibus tincidunt ornare volutpat.
+                    </p>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
                     <div className="absolute bottom-6 right-6">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
