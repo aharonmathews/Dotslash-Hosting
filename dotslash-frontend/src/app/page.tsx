c265d11ea02d5059c2d67e755c06fd3b7f137eae
Aharon Mathews <121443448+aharonmathews@users.noreply.github.com>
2025-03-06 15:43:45 +0530

    Coming Soon update

--- dotslash-frontend/src/app/page.tsx
+++ dotslash-frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
     <div className="flex flex-col items-center justify-center h-screen w-full">
-      You can start working here
+      Coming Soon
     </div>
     </>
   );
