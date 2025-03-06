d55a7987b99c34a0d05a59a83ff277793885b037
Roshin R <139961141+RoshinR2005@users.noreply.github.com>
2025-03-06 23:22:18 +0530

    Added gallery section

--- dotslash-frontend/src/app/page.tsx
+++ dotslash-frontend/src/app/page.tsx
@@ -1,11 +1,15 @@
 import Welcome from "./components/welcome";
 import Countdown from "./components/countdown";
+import Gallery from "./components/gallery";
+
+
 export default function Home() {
   return (
     <>
     <div className="flex flex-col w-full gap-10">
       <Welcome/>
       <Countdown/>
+      <Gallery/>
     </div>
     </>
   );
