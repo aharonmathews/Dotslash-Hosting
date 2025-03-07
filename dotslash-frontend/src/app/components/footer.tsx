8735721579ff88abb3a615c90d1f6d10b7700333
Ivin <ivinmk2410@hotmail.com>
2025-03-07 10:53:50 +0530

    Completed Footer

---
+++ dotslash-frontend/src/app/components/footer.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import dotslash from "../assets/dotslash.svg"
+import Image from 'next/image'
+import { Noto_Sans, Unbounded } from 'next/font/google';
+
+
+const unbounded = Unbounded({ weight: '400', style: 'normal', preload:false });
+const notoSans = Noto_Sans({ weight: '400', style: 'normal', preload:false });
+
+const Footer = () => {
+  return (
+    <div className='bg-[#D1A83A] w-screen px-[20px] lg:px-[54px] pt-[50px] text-black'>
+
+        <div className='flex flex-col gap-[38px] lg:flex-row lg:items-start lg:gap-[350px] lg:mb-[120px]'>
+            <Image src={dotslash} alt="dotslash"/>
+            <div className='flex flex-wrap md:flex-row gap-x-[80px] gap-y-[48px] lg:gap-x-[143px] mb-5'>
+                <div className={`flex flex-col gap-[16px]  text-[16px] ${notoSans.className}`}>
+                    <p>Home</p>
+                    <p>Events</p>
+                    <p>Gallery</p>
+                </div>
+                <div className={`flex flex-col gap-[16px]  text-[16px] ${notoSans.className}`}>
+                    <p>For Clubs</p>
+                    <p>For Sponsors</p>
+                    <p>Get T-Shirt</p>
+                </div>
+                <div>
+                    Contact:
+                </div>
+            </div>
+        </div>
+
+        <div className={`text-[39px] lg:text-[96px] ${unbounded.className} uppercase leading-tight w-full py-[30px] border-b-[1px] border-b-black`}>
+            The power <br/> to <u>create</u>
+        </div>
+        <div className='w-full flex flex-row items-center justify-center uppercase py-[20px]'>
+            Dotslash CET
+        </div>
+    </div>
+  )
+}
+
+export default Footer
