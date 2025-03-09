cbf3dc8bd8cf5b9611fa580a126e78193cb7748d
Noel George C J <noelgeorge2004@gmail.com>
2025-03-09 19:49:33 +0530

    T shirt section

--- dotslash-frontend/src/app/components/Tshirt.tsx
+++ dotslash-frontend/src/app/components/Tshirt.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import TshirtImage from "../assets/tshirt.png";
+import TshirtImage from "../assets/dotshirt.png";
 import Tbackground from "../assets/Tbackground.png";
 import { Unbounded} from "next/font/google";
 import ShaderCanvas from "./shaderCanvas";
@@ -9,20 +9,20 @@ const unbounded = Unbounded({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function Tshirt() {
   return (
-    <div className="relative md:w-full h-screen inset-0 flex flex-col items-start justify-start text-white ">
+    <div className="relative z-10 md:w-full h-screen inset-0 flex flex-col items-start justify-start text-white ">
       <div className="absolute inset-0 md:w-full w-[600px] md:h-[800px] h-[1000px] ">
         <ShaderCanvas image="/Tbackground.png" />
-        <div className="absolute inset-0 bg-black opacity-40 "></div>
+        <div className="absolute inset-0 w-screen bg-black opacity-40 "></div>
       </div>
 
-      <div className="relative flex md:flex-row flex-col justify-between max-w-10xl p-6 h-full w-full pt-16 mt-20">
-        <div className="md:w-[70%] w-[300px] text-left mb-10  md:ml-10 md:pl-10">
-          <h1 className={`md:text-8xl text-2xl ${unbounded.className} mb-5`}>
+      <div className="relative flex md:flex-row flex-col justify-between max-sm:py-0 p-6 h-full w-full pt-16 mt-7 md:mt-20">
+        <div className="text-left mb-10  md:ml-10 md:pl-10 h-full">
+          <h1 className={`md:text-6xl xl:text-8xl text-4xl ${unbounded.className} mb-5`}>
             GRAB YOUR <br />
             <span className="md:mt-5 block">TEES</span>
           </h1>
 
-          <p className="text-xs leading-relaxed mt-10  md:w-[43%] w-[350px]">
+          <p className="text-slate-200 text-sm lg:text-base leading-relaxed mt-4 md:mt-10  md:w-[43%] w-[350px]">
             Lorem ipsum dolor sit amet consectetur. Ut nec gravida nulla justo.
             Dui lectus venenatis ut nisl. Ut eu vivamus turpis commodo vitae
             viverra.. Auctor vel ut sagittis viverra. Sollicitudin interdum amet
@@ -37,13 +37,11 @@ export default function Tshirt() {
           <HoverButton />
         </div>
 
-        <div className="md:w-[30%] w-[450px] flex justify-center items-center md:mb-60 ">
+        <div className="md:w-[45%] w-[450px] flex justify-center absolute max-sm:top-[550px] max-sm:right-[50vw] max-sm:translate-x-1/2 right-0 top-1/2 -translate-y-1/2 md:mb-60 ">
           <Image
             src={TshirtImage}
             alt="T-shirt"
-            width={600}
-            height={600}
-            className="object-contain md:max-w-[580px] md:max-h-[580px] mr-20 max-w-[450px] max-h-[450px]"
+            className="object-contain lg:max-w-[780px] lg:max-h-[780px] md:mr-20 md:max-w-[450px] max-w-[300px]"
           />
         </div>
       </div>
