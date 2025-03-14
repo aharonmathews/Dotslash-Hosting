43b1878a9dfeb9c05fef5937dc760f62b33d4299
Abhishek <abhisheksathyan483@gmail.com>
2025-03-14 23:46:21 +0530

    Update Landing.tsx
    landing finshed

--- dotslash-frontend/src/app/components/Landing.tsx
+++ dotslash-frontend/src/app/components/Landing.tsx
@@ -3,6 +3,7 @@ import { Unbounded, Noto_Sans } from "next/font/google";
 const unbounded = Unbounded({ weight: "400", style: "normal", preload: false });
 const noto_sans = Noto_Sans({ weight: "400", style: "normal", preload: false });
 
+
 const Landing = () => {
   return (
     <div className="landing-container relative flex justify-center mt-20 pt-10">
@@ -20,14 +21,14 @@ const Landing = () => {
          
 
         </video>
-        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center text-8xl md:text-5xl font-unbounded drop-shadow-lg">
-         <h2 className="text-8xl"><span className="mr-80 pr-40 mt-40 inline-block" > The</span>  <span className="mr-20 pt-10 inline-block">Singularity</span> <span className="pt-10 inline-block ml-20">of  </span> <span className="pt-10 ml-40 inline-block">Innovation</span> </h2>
+        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center text-8xl md:text-4xl font-unbounded drop-shadow-lg">
+         <h2 className="text-3xl lg:text-8xl sm:text-4xl md:text-5xl"><span className="lg:mr-80    pr-[125px] lg:pr-40 md:mr-20 sm:pr-20 mt-20 lg:mt-40 md:mt-20 sm:mt-20  inline-block" > The</span>  <span className="lg:mr-20  lg:pt-10  inline-block">Singularity</span> <span className="lg:pt-10 inline-block ml-20">of  </span> <span className="lg:pt-10 lg:ml-40 inline-block">Innovation</span> </h2>
         </div>
         
-        <div className="absolute top-0 left-0 w-40 h-40 border-t-4 border-l-4 border-[#D1A83A]"></div>
-        <div className="absolute top-0 right-0 w-40 h-40 border-t-4 border-r-4 border-[#D1A83A]"></div>
-        <div className="absolute bottom-0 left-0 w-40 h-40 border-b-4 border-l-4 border-[#D1A83A]"></div>
-        <div className="absolute bottom-0 right-0 w-40 h-40 border-b-4 border-r-4 border-[#D1A83A]"></div>
+        <div className="absolute top-0 left-0 w-16 h-16 lg:w-40 lg:h-40 lg:w-40 lg:h-40 md:w-10 md:h-10 border-t-4 border-l-4 border-[#D1A83A]"></div>
+        <div className="absolute top-0 right-0 w-16 h-16 lg:w-40 lg:h-40 md:w-10 md:h-10 border-t-4 border-r-4 border-[#D1A83A]"></div>
+        <div className="absolute bottom-0 left-0 w-16 h-16 lg:w-40 lg:h-40 md:w-10 md:h-10 border-b-4 border-l-4 border-[#D1A83A]"></div>
+        <div className="absolute bottom-0 right-0 w-16 h-16 lg:w-40 lg:h-40 md:w-10 md:h-10 border-b-4 border-r-4 border-[#D1A83A]"></div>
       </div>
     </div>
   );
