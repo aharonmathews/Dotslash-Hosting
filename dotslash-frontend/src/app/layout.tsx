19f3f2593f2c9d2b44363ce9396b02bcd1f368fb
Ivin Mathew Kurian <127372364+Ivin-Mathew@users.noreply.github.com>
2025-03-14 21:23:46 +0530

    Update layout.tsx
    Added SEO tags

--- dotslash-frontend/src/app/layout.tsx
+++ dotslash-frontend/src/app/layout.tsx
@@ -15,6 +15,20 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "DotSlash 2025",
   description: "The official website for DotSlash 2025, the techno-cultural fest of CSE CET",
+  openGraph: {
+    title: "DotSlash 2025",
+    description: "The official website for DotSlash 2025, the techno-cultural fest of CSE CET",
+    url: "https://dotslashcet.in/", // Choose your primary URL here
+    type: "website",
+    images: "https://i.postimg.cc/qBPjg18z/card-Image.jpg",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "DotSlash 2025",
+    description: "The official website for DotSlash 2025, the techno-cultural fest of CSE CET",
+    images: "https://i.postimg.cc/qBPjg18z/card-Image.jpg",
+    site: "dotslashcet.in",
+  },
 };
 
 export default function RootLayout({
